e44a1cff6d9304f230dd085dc025e37a90245272
nfrappart <61788760+nfrappart@users.noreply.github.com>
2021-01-15 17:06:38 +0100

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+############################
+# Module for VNet creation #
+############################
+
+
+#Creating a VNet
+resource "azurerm_virtual_network" "Terra_VNet" {
+  name                = var.VNetName
+  resource_group_name = var.RgName
+  address_space       = var.VNetAddressSpace
+  #dns_servers         = var.VNetDnsList
+  location            = var.VNetLocation
+
+  tags = {
+    Environment       = var.EnvironmentTag
+    Usage             = var.EnvironmentUsageTag
+    Owner             = var.OwnerTag
+    ProvisioningDate  = var.ProvisioningDateTag
+    ProvisioningMode    = var.ProvisioningModeTag
+  }
+  lifecycle {
+    ignore_changes = [
+      tags["ProvisioningDate"],
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,55 @@
+##########################
+# Module for RG creation #
+##########################
+
+
+#Variable declaration for Module
+
+variable "VNetName" {
+  type    = string
+  default = "DefaultVNet"
+}
+
+variable "RgName" {
+  type    = string
+  default = "DefaultRSG"
+}
+
+variable "VNetLocation" {
+  type    = string
+  default = "westeurope"
+}
+
+variable "VNetAddressSpace" {
+  type    = list
+  default = ["10.0.0.0/20"]
+}
+
+variable "VNetDnsList" {
+  type    = list
+}
+
+variable "EnvironmentTag" {
+  type    = string
+  default = "Sandbox"
+}
+
+variable "EnvironmentUsageTag" {
+  type    = string
+  default = "Poc usage only"
+}
+
+variable "OwnerTag" {
+  type    = string
+  default = "Nate"
+}
+
+variable "ProvisioningDateTag" {
+  type    = string
+  default = "Today :)"
+}
+
+variable "ProvisioningModeTag" {
+  type    = string
+  default = "Terraform"
+}
